ba6fa68c252096510a59282d5a1e065c683ce117
a213262-Arthur Galvão <a213262@dac.unicamp.br>
2024-09-02 19:43:25 -0300

    .

--- RodarPrograma.bat
+++ RodarPrograma.bat
@@ -1,1 +1,11 @@
-Rscript main.R
+@echo off
+set "R_PATH=C:\Program Files\R\R-4.3.1\bin\R.exe"
+if not exist "%R_PATH%" (
+    echo O R.exe nao foi encontrado no caminho especificado.
+    pause
+    exit /b 1
+)
+
+"%R_PATH%" CMD BATCH "%~dp0main.R"
+
+exit
